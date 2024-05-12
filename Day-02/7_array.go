a78f81ec55190b81c4dce7656a1d97733449e488
Praduman Prajapati <praduman.cnd@gmail.com>
2024-05-12 07:26:49 +0530

    _

--- Day-02/7_array.go
+++ Day-02/7_array.go
@@ -6,4 +6,7 @@ func main() {
 	fmt.Println("Array:")
 	var o [5]int = [5]int{1, 2, 3, 4, 5} // array of five integers
 	fmt.Println("o=", o)
+
+	var m [5]int = [5]int{1, 2, 3, 4} // array of five integers
+	fmt.Println("o=", m)
 }
--- Day-02/8_slice.go
+++ Day-02/8_slice.go
@@ -6,4 +6,7 @@ func main() {
 	fmt.Println("Slice:")
 	p := []int{1, 2, 3, 4, 5} // slice of integers
 	fmt.Println("p=", p)
+
+	j := []int{1, 5, 76, 8 , 2, 3, 4, 5} // slice of integers
+	fmt.Println("p=", j)
 }
